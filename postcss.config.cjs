17b9d08320233971aeb97d4571ae37e0b437e829
sc9632 <1356422517@qq.com>
2023-10-25 11:26:23 +0800

    feat: 添加注释

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,15 +1,17 @@
 module.exports = {
   plugins: {
+    //向CSS规则添加浏览器前缀
     autoprefixer: {
       overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8', 'last 10 versions'],
       grid: true,
     },
+    //将CSS中的像素单位(px)转换为rem单位
     'postcss-pxtorem': {
       rootValue({ file }) {
-        return file.indexOf('vant') !== -1 || file.indexOf('scm-') !== -1 ? 37.5 : 75
+        return file.indexOf('vant') !== -1 ? 37.5 : 75 //这个值会作为rem的根值，用于将所有的px单位转换为rem单位。
       },
       propList: ['*'],
-      unitPrecision: 5,
+      unitPrecision: 5, //单位转换的精度。例如，如果unitPrecision是5，那么1px会被转换为1.00000rem。
     },
   },
 }
